cc5a30a121ae4a47c15805097a28b8ae037a9b64
Mateusz.Galasinski <mateusz.galasinski@protonmail.com>
2019-01-09 23:12:00 +0100

    added env file to build process

--- build_and_run_image_api.ps1
+++ build_and_run_image_api.ps1
@@ -1,3 +1,3 @@
 docker build -t vote-manager/api:master .
 docker rm -f vote_api
-docker run -p 3010:3010 --name vote_api vote-manager/api:master
+docker run -p 3010:3010 --env-file api.env --name vote_api vote-manager/api:master
